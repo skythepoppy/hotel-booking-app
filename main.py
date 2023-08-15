1324b922c325f2d3f49cf7991b6ff3de56c8788e
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-15 14:53:12 -0500

    created classes, instances, and added an if-else loop for hotel availability and generating a ticket

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import pandas
 
+df = pandas.read_csv("hotels.csv")
 class Hotel:
+    def __init__(self,id):
+        pass
     def book(self):
         pass
 
+    def available(self):
+        pass
 
 
 class ReservationTicket:
+    def __init__(self, user_name, hotel_obj):
+        pass
+
     def generate(self):
         pass
+
+
+print(df)
+id = input("Enter the ID of the hotel: ")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    user_name = input("Please enter your name: ")
+    reservation_ticket = ReservationTicket(user_name, hotel)
+    reservation_ticket.generate()
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not free")
